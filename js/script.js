f14eeb45bd93e01e9b972d967d7639ee239a655c
Baptiste <baptiste.tarrit2@gmail.com>
2023-03-24 00:07:12 +0100

    ça avance
    fini le style global
    plus que a jouter une ou deux page projet en plus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+if (document.getElementById('contact_button')) {
+  document.getElementById('contact_button').addEventListener('click', () => {
+    document.getElementById('icons').scrollIntoView({behavior: "smooth"})
   })
 }
